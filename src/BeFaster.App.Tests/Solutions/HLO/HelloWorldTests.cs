242ce15db3d065201e8d335518ef7ee0e5d9bee2
sbx_user1059 <sbx_user1059@ip-10-129-35-206.eu-west-2.compute.internal>
2019-01-19 23:45:39 +0000

    Sat Jan 19 23:45:39 UTC 2019

--- src/BeFaster.App.Tests/Solutions/HLO/HelloWorldTests.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloWorldTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeFaster.App.Solutions.HLO;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Tests.Solutions.HLO
 {
+    [TestFixture]
     class HelloWorldTests
     {
+        [TestCase("friend", ExpectedResult = "hello to the world")]
+        public string HellowWorld(string friendName)
+        {
+            return HelloSolution.Hello(friendName);
+        }
     }
 }
+
